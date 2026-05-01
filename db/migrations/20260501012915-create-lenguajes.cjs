2da1c55339069c2aab8827ddef10d2cc3903fe3b
Bryan <bryanribero@hotmail.com>
2026-05-01 01:42:37 -0300

    Ajustando la migracion de lenguajes para que contenga un default en createdAt y updatedAt y a su vez agregandole un trigger para que se Actualize el updatedAt a nivel de base de datos

--- db/migrations/20260501012915-create-lenguajes.cjs
+++ db/migrations/20260501012915-create-lenguajes.cjs
@@ -5,7 +5,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('lenguajes', {
-      id_lenguajes: {
+      id_lenguaje: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
@@ -19,15 +19,42 @@ module.exports = {
       createdAt: {
         type: Sequelize.DATE,
         allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
       updatedAt: {
         type: Sequelize.DATE,
         allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
     })
+
+    await queryInterface.sequelize.query(`
+      CREATE OR REPLACE FUNCTION set_updated_at()
+      RETURNS TRIGGER AS $$
+      BEGIN
+        NEW."updatedAt" = CURRENT_TIMESTAMP;
+        RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+      `)
+
+    await queryInterface.sequelize.query(`
+        CREATE TRIGGER trigger_set_updated_at
+        BEFORE UPDATE ON "lenguajes"
+        FOR EACH ROW
+        EXECUTE FUNCTION set_updated_at();
+        `)
   },
 
   async down(queryInterface) {
+    await queryInterface.sequelize.query(`
+      DROP TRIGGER IF EXISTS trigger_set_updated_at ON lenguajes;
+      `)
+
+    await queryInterface.sequelize.query(`
+      DROP FUNCTION IF EXISTS set_updated_at;
+      `)
+
     await queryInterface.dropTable('lenguajes')
   },
 }
