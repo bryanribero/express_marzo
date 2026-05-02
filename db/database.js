3606ad59e55fcb97f5e680138c8fac74a176dc69
Bryan <bryanribero@hotmail.com>
2026-05-02 03:32:15 -0300

    probando

--- db/database.js
+++ db/database.js
@@ -13,12 +13,6 @@ if (process.env.DB_URL) {
   sequelize = new Sequelize(process.env.DB_URL, {
     dialect: 'postgres',
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   })
 } else {
   sequelize = new Sequelize(
